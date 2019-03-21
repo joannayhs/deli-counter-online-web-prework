f5960a0eff1f7db4d301606c0a8b539304b98357
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-21 20:01:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+katz_deli=[]
+
+def line(katz_deli)
+  if katz_deli.size == 0
+   puts "The line is currently empty."
+  else 
+     string = "The line is currently:"
+     katz_deli.each_with_index do |name, index|
+     string << " #{katz_deli.index(name)+1}. #{name}"
+   end 
+    puts string
+  end 
+ end 
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
